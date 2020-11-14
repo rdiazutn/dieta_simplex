787d6b82fe221e0b5d7449febd2b55e47f19e43f
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2020-11-14 12:00:09 -0300

    Fixes to deploy

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,24 +2,18 @@
   <v-app dark class="w-75vw">
     <v-row justify="center">
       <v-col cols="10">
-        <CompanyLogo src="/chrome.png" size="big" />
+        <CompanyLogo src="/dieta_simplex/chrome.png" size="big" />
         <h1>Oops...</h1>
         <p>{{ $t('errors.oops') }}</p>
-        <ThePrimaryButton
-          :inner-text="$t('errors.go_to_start')"
-          @click="goToLogin"
-        />
       </v-col>
     </v-row>
   </v-app>
 </template>
 
 <script>
-import ThePrimaryButton from '~/components/General/Buttons/ThePrimaryButton'
 import CompanyLogo from '~/components/General/CompanyLogo'
 export default {
   components: {
-    ThePrimaryButton,
     CompanyLogo
   },
   methods: {
